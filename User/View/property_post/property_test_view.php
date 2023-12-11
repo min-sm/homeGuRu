7be3d913138ca2a4babeba15bde61068836907fa
DESKTOP-RCC3Q87\User <minsoemoe999@gmail.com>
2023-12-11 13:51:47 +0630

    some minor indentation changes

--- User/View/property_post/property_test_view.php
+++ User/View/property_post/property_test_view.php
@@ -56,6 +56,8 @@
 
   <!-- font awesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  <!-- JS -->
   <script src="../resources/js/property_detail.js" defer></script>
 
 </head>
