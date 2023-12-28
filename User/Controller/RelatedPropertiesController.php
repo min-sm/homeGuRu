1bedba7527c22339ac0ad08c3dc97d1747346419
zukoackerman <kkzedn99@gmail.com>
2023-12-28 23:06:02 +0630

    blah blah blah

--- User/Controller/RelatedPropertiesController.php
+++ User/Controller/RelatedPropertiesController.php
@@ -16,10 +16,14 @@ $sql = $pdo->prepare("SELECT p.*, pt.*,mt.*,mc.*
 FROM properties p 
 JOIN property_type pt ON p.pt_id = pt.id
 JOIN m_townships mt ON p.p_township=mt.id
-JOIN m_collaborators mc ON p.uploader_id=mc.id
+JOIN m_collaborators mc ON p.uploader_id
 WHERE pt_id = :pt_id 
 OR p_offer = :p_offer 
-OR p_township = :p_township");
+OR p_township = :p_township
+AND p.p_status=1 
+AND mc.gc_activity_ban=0 
+ORDER BY p.id DESC
+");
 
 // Bind the values to the placeholders
 $sql->bindValue(":pt_id", $pt_id);
