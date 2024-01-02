ef69c305284ddcb7f00192fad118174e74aaa758
DESKTOP-RCC3Q87\User <minsoemoe999@gmail.com>
2024-01-03 01:48:57 +0630

    all stock out of stock in stock changes made

--- Admin/Controller/Property/ListOutOfStockController.php
+++ Admin/Controller/Property/ListOutOfStockController.php
@@ -5,13 +5,13 @@ $page = isset($_GET['page']) ? $_GET['page'] : 1;
 $recordsPerPage = 6;
 $offset = ($page - 1) * $recordsPerPage;
 
-$query = "SELECT p.*, pt.pt_name AS pt_name, township.name AS township_name FROM properties p, property_type pt, m_townships township WHERE p.del_flg = 0 AND p.pt_id = pt.id AND p.p_after = 1 AND township.id = p.p_township ORDER BY p.id LIMIT $recordsPerPage OFFSET $offset";
+$query = "SELECT p.*, pt.pt_name AS pt_name, township.name AS township_name FROM properties p, property_type pt, m_townships township WHERE p.del_flg = 0 AND p.pt_id = pt.id AND p.p_after = 1 AND township.id = p.p_township AND p.p_status = 2 ORDER BY p.id LIMIT $recordsPerPage OFFSET $offset";
 
 $sql = $pdo->prepare($query);
 $sql->execute();
 $properties = $sql->fetchAll(PDO::FETCH_ASSOC);
 
-$queryForResultCount = "SELECT COUNT(p.id) AS total_result FROM properties p WHERE p.del_flg = 0 AND p.p_after = 1";
+$queryForResultCount = "SELECT COUNT(p.id) AS total_result FROM properties p WHERE p.del_flg = 0 AND p.p_after = 1 AND p.p_status = 2";
 $sqlForResultCount = $pdo->prepare($queryForResultCount);
 $sqlForResultCount->execute();
 $resultCount = $sqlForResultCount->fetch(PDO::FETCH_ASSOC);
--- Admin/Controller/Property/PropertyListController.php
+++ Admin/Controller/Property/PropertyListController.php
@@ -5,14 +5,14 @@ $page = isset($_GET['page']) ? $_GET['page'] : 1;
 $recordsPerPage = 6;
 $offset = ($page - 1) * $recordsPerPage;
 
-$query = "SELECT p.*, pt.pt_name, township.name AS township_name FROM properties p, property_type pt, m_townships township WHERE p.del_flg = 0 AND p.pt_id = pt.id AND p.p_township = township.id ORDER BY p.id LIMIT $recordsPerPage OFFSET $offset";
+$query = "SELECT p.*, pt.pt_name, township.name AS township_name FROM properties p, property_type pt, m_townships township WHERE p.del_flg = 0 AND p.pt_id = pt.id AND p.p_township = township.id AND p.p_status = 2 ORDER BY p.id LIMIT $recordsPerPage OFFSET $offset";
 
 $sql = $pdo->prepare($query);
 $sql->execute();
 $properties = $sql->fetchAll(PDO::FETCH_ASSOC);
 // used in list.php 
 
-$queryForResultCount = "SELECT COUNT(p.id) AS total_result FROM properties p WHERE p.del_flg = 0";
+$queryForResultCount = "SELECT COUNT(p.id) AS total_result FROM properties p WHERE p.del_flg = 0 AND p.p_status = 2";
 $sqlForResultCount = $pdo->prepare($queryForResultCount);
 $sqlForResultCount->execute();
 $resultCount = $sqlForResultCount->fetch(PDO::FETCH_ASSOC);
